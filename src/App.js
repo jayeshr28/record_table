42a6d449a0653856d93e149b1b2b182f3b9cb007
jayeshr28 <jayeshrathore2810@gmail.com>
2023-06-21 17:16:42 +0530

    main

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import UserTable from './components/Task4';
+
+function App() {
+  return (
+    <>
+    
+<div>
+      <h1>User Records</h1>
+      <UserTable />
+    </div>
+    
+    </>
+    
+  );
+}
+
+export default App;
